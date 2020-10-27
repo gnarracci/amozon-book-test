445d6342d46ad32f7647e9661f4f7ef00f92a52b
Gianni Narracci <gnarracci@gmail.com>
2020-10-27 10:15:19 -0400

    Menu & List Component Setup

--- src/Menu.js
+++ src/Menu.js
@@ -2,6 +2,16 @@ import React, { Component } from "react";
 
 export default class Menu extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div className="container">
+        <div className="subcontainer">
+          <div className="logo">{this.props.title}</div>
+
+          <div className="search"></div>
+
+          <div className="actions"></div>
+        </div>
+      </div>
+    );
   }
 }
